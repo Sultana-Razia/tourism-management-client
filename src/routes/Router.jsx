91c749a9d1f59a0d129335e077c9dba09be4f55b
Sultana Razia <sultanarazia483@gmail.com>
2024-10-02 09:52:06 +0600

    Route setup

---
+++ src/routes/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layouts/Root";
+import Home from "../pages/Home/Home";
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        // errorElement:
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+
+export default router;
