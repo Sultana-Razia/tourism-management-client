987ffd81a97d0210d3371332f0ce93b34b545c37
Sultana Razia <sultanarazia483@gmail.com>
2024-10-03 11:52:31 +0600

    Integrate login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProviders';
 
 const Login = () => {
+
+    const { signIn, handleGoogleSignIn, handleGithubSignIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleLogin = e => {
+        e.preventDefault();
+
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const password = form.get('password');
+        // console.log(email, password);
+        signIn(email, password)
+            .then(result => {
+                console.log(result.user);
+
+                //navigate after login
+                navigate(location?.state ? location.state : '/');
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+
+
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen rounded-md mt-6">
@@ -10,26 +39,34 @@ const Login = () => {
                         <p className="py-6">Welcome back! Please enter your credentials to log into your account.</p>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <form className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <p>Do not have an account please <Link to='/register' className="text-blue-500 font-bold">Register</Link></p>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
                         </form>
+                        <p className="text-center text-3xl font-semibold">or</p>
+                        <div className="form-control mt-2 px-7">
+                            <button onClick={handleGoogleSignIn} className="btn btn-primary">Continue with Google</button>
+                        </div>
+                        <p className="text-center text-3xl font-semibold">or</p>
+                        <div className="form-control my-2 px-7">
+                            <button onClick={handleGithubSignIn} className="btn btn-primary">Continue with Github</button>
+                        </div>
                     </div>
                 </div>
             </div>
