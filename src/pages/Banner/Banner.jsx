46f39cb558d8b91cdbe5c3d14e75dcdfc4a4227e
Sultana Razia <sultanarazia483@gmail.com>
2024-11-14 09:14:08 +0600

    Most popular section added in Home page

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -4,8 +4,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import image1 from '../../assets/images/card1.jpg'
-import image2 from '../../assets/images/card2.jpg'
+// import image1 from '../../assets/images/card1.jpg'
+// import image2 from '../../assets/images/card2.jpg'
 
 const Banner = () => {
 
@@ -24,22 +24,28 @@ const Banner = () => {
         <div className='w-[80%] mx-auto mt-3'>
             <Slider {...settings}>
                 <div>
-                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md" src={image1} alt="" />
+                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md"
+                        src="https://www.rjtravelagency.com/wp-content/uploads/2023/09/Indonesia.jpg" alt="Indonesia" />
                 </div>
                 <div>
-                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md" src={image2} alt="" />
+                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md"
+                        src="https://d1shf7n3qgesaf.cloudfront.net/wp-content/uploads/20240521155530/BALCountryOverview_Malaysia.webp" alt="Malaysia" />
                 </div>
                 <div>
-                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md" src={image1} alt="" />
+                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md"
+                        src="https://www.ana.co.jp/www2/plan-book/where-we-travel/singapore/singapore-02.jpg" alt="Singapore" />
                 </div>
                 <div>
-                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md" src={image1} alt="" />
+                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md"
+                        src="https://www.expat.hsbc.com/content/dam/hsbc/mbos/img/international-banking/16-9/3347-thailand-2-2000x1125.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg" alt="Thailand" />
                 </div>
                 <div>
-                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md" src={image2} alt="" />
+                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md"
+                        src="https://kampatour.com/pic/news/1b26bb3d-1cb7-4ad1-b161-dcf194e08ba6.jpg" alt="Vietnam" />
                 </div>
                 <div>
-                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md" src={image1} alt="" />
+                    <img className="h-[300px] md:h-[400px] lg:h-[500px] w-full rounded-md"
+                        src="https://assets.airtrfx.com/media-em/library/countries/philippines-PH.jpg" alt="Philippines" />
                 </div>
             </Slider>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,35 @@ import React from 'react';
 import Navbar from '../../Shared/Navbar/Navbar';
 import Banner from '../Banner/Banner';
 import Footer from '../../Shared/Footer/Footer';
+import { Link, useLoaderData } from 'react-router-dom';
+import TouristSpotCard from '../../components/TouristSpotCard';
 
 const Home = () => {
+
+    const allTouristSpots = useLoaderData();
+
     return (
         <div>
             <Navbar></Navbar>
             <Banner></Banner>
-
+            <div className='w-[80%] mx-auto'>
+                <div className='text-center mt-24'>
+                    <h2 className='lobster-regular text-3xl text-[#E8604C]'>Most Popular Tours</h2>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+                    {
+                        allTouristSpots.slice(0, 3).map(allTouristSpot => <TouristSpotCard
+                            key={allTouristSpot._id}
+                            allTouristSpot={allTouristSpot}
+                        ></TouristSpotCard>)
+                    }
+                </div>
+                <div className='mt-10 text-center'>
+                    <Link className='' to={'/allTouristSpot'}><button
+                        className='text-white bg-[#E8604C] px-4 py-2 rounded-md text-xl font-medium'
+                    >See More</button></Link>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
